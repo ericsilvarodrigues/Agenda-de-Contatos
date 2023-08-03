c755ccadca19d589703d5a8b54a5250e3e9f418f
Eric Silva Rodrigues <95319493+ericsilvarodrigues@users.noreply.github.com>
2023-08-03 19:24:46 -0300

    Add files via upload

---
+++ agenda/src/main/webapp/scripts/confirmador.js
@@ -0,0 +1,13 @@
+/**
+ * Confirmar a exclusão de um contato 
+ */
+
+function confirmar(idcon) {
+	let resposta = confirm("confirma a exclusão deste contato?")
+	if (resposta === true) {
+		
+	//alert(idcon)
+	window.location.href= "delete?idcon=" + idcon 
+	}
+
+}
